2946b7d5e054cf9617640d00b368d7334ac23cc9
inqu0302 <inqu0302@naver.com>
2021-09-14 15:00:15 +0900

    버킷리스트 만들기, 몽고db연동

---
+++ react-nodejs-api/README.md
@@ -0,0 +1,4 @@
+# react와 nodejs 연동
+
+- 서로 다른 어플끼리 데이터를 주고받는 관계설정
+- localhost:3000 - localhost:5000 연동과정에서 cors 문제가 발생
---
+++ react-nodejs-api/routes/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const BUCKET = require("../models/bucket");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+const bucketList = {
+  b_id: "0001",
+  b_flag: 0,
+  b_title: "Nodejs 연동",
+  b_start_date: "2021-09-14 00:12:32",
+  b_end_date: "",
+  b_end_check: false,
+  b_cancel: false,
+};
+
+// react로부터 데이터를 받아서 mongoDB에 insert 수행하기
+router.post("/insert", async (req, res) => {
+  const result = await BUCKET.create(req.body);
+  await res.json(result);
+});
+
+router.get("/data", async (req, res) => {
+  const result = await BUCKET.find({});
+  res.json(result);
+});
+module.exports = router;
